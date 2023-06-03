3418e80c061b9252b4b455576a88167d3dee3818
zomeru <zomerzxc.19@gmail.com>
2023-06-03 17:42:19 +0800

    Some tweaks

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,7 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 import * as AuthSession from 'expo-auth-session';
 import Toast from 'react-native-toast-message';
 import Constants from 'expo-constants';
-import * as Linking from 'expo-linking';
 
 const SUPABASE_URL = Constants.expoConfig?.extra?.SUPABASE_URL;
 const SUPABASE_SERVICE_KEY = Constants.expoConfig?.extra?.SUPABASE_SERVICE_KEY;
@@ -33,31 +32,39 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY, {
 type OAuthProvider = 'google' | 'github' | 'discord';
 
 export const oAuthLogin = async (provider: OAuthProvider) => {
-  const redirectUrl = Linking.createURL('expo-auth-session');
-
-  console.log({
-    REDIRECT_URL: redirectUrl,
-    REDIRECT_URL2: redirectUrl,
-    REDIRECT_URL3: redirectUrl,
+  const redirectUrl = AuthSession.makeRedirectUri({
+    useProxy: false,
   });
 
   const authUrl = `${SUPABASE_URL}/auth/v1/authorize?provider=${provider}&redirect_to=${redirectUrl}&prompt=consent`;
 
-  const authSessionResponse = await AuthSession.startAsync({
+  AuthSession.startAsync({
     authUrl,
     returnUrl: redirectUrl,
-  });
+  })
+    .then((response) => {
+      console.log('response', response);
 
-  console.log({
-    AUTHRESPONSE: authSessionResponse,
-  });
+      if (response.type === 'success') {
+        supabase.auth.setSession({
+          access_token: response.params.access_token,
+          refresh_token: response.params.refresh_token,
+        });
+      }
+    })
+    .catch((error) => {
+      console.log('error signin in', {
+        error,
+        message: error.message,
+      });
+    });
 
-  if (authSessionResponse.type !== 'success') return;
+  // if (authSessionResponse.type !== 'success') return;
 
-  supabase.auth.setSession({
-    access_token: authSessionResponse.params.access_token,
-    refresh_token: authSessionResponse.params.refresh_token,
-  });
+  // supabase.auth.setSession({
+  //   access_token: authSessionResponse.params.access_token,
+  //   refresh_token: authSessionResponse.params.refresh_token,
+  // });
 };
 
 export const handleSupabaseResendOTP = async (
